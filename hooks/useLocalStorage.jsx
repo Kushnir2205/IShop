fb47e6d88242d4f3d458d758a2adb5d2e51d6bbc
Kushnir2205 <kushnirroman22@gmail.com>
2023-11-08 22:51:54 +0200

    fix err

--- hooks/useLocalStorage.jsx
+++ hooks/useLocalStorage.jsx
@@ -3,8 +3,10 @@ import { useEffect, useState } from "react";
 
 export const useLocalStorage = (key, defData) => {
   const [state, setState] = useState(() => {
-    const localData = localStorage.getItem(key);
-    return localData ? JSON.parse(localData) : defData;
+    if (typeof window !== "undefined") {
+      const localData = localStorage.getItem(key);
+      return localData ? JSON.parse(localData) : defData;
+    }
   });
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(state));
@@ -17,10 +19,8 @@ export const useLocalStorage = (key, defData) => {
 
 // export const useLocalStorage = (key, defData) => {
 //   const [state, setState] = useState(() => {
-//     if (typeof window !== "undefined") {
-//       const localData = localStorage.getItem(key);
-//       return localData ? JSON.parse(localData) : defData;
-//     }
+//     const localData = localStorage.getItem(key);
+//     return localData ? JSON.parse(localData) : defData;
 //   });
 //   useEffect(() => {
 //     localStorage.setItem(key, JSON.stringify(state));
