88278a4a8b47267ae30918f7cac4d2bb2765ccce
Vyachuk <Slavkoka045@gmail.com>
2023-11-10 16:37:18 +0200

    ADD: burger search styles

--- components/BurgerMenu/BurgerMenu.jsx
+++ components/BurgerMenu/BurgerMenu.jsx
@@ -1,44 +1,19 @@
-"use client";
-import React, { useState } from "react";
-import { TfiClose } from "react-icons/tfi";
 import s from "./BurgerMenu.module.css";
-import ButtonTheme from "../ButtonTheme/ButtonTheme";
-import ListNav from "../ListNav/ListNav";
-import { RiMenu2Line } from "react-icons/ri";
+
 import Search from "../Search/Search";
-import Link from "next/link";
-import { BsSearch } from "react-icons/bs";
-import { HiShoppingCart } from "react-icons/hi";
 
 const BurgerMenu = ({ children }) => {
   return (
-    <div className={s.modalBackDrop}>{children}</div>
-    //<>
-    /* <div className={s.backDrop}>
-        <div className={s.burgerHeader}>
-          <div onClick={toggleMenu} className={s.menuBtn}>
-            {isOpen ? (
-              <TfiClose className={s.closeIcon} />
-            ) : (
-              <div className={s.burgerLogo}>
-                <RiMenu2Line className={s.burgerBtn} />
-                <Link className={s.logo} href="/">
-                  iShop
-                </Link>
-              </div>
-            )}
-          </div>
-          {isOpen && <Search />}
-        </div> }
-        { {isOpen && (
-          <>
-            <ListNav isOpen={isOpen} />
-            <ButtonTheme />
-            <p>Dark theme</p>
-          </>
-        )}
-      </div> */
-    //</>
+    <div className={s.modalBackDrop}>
+      <div className={s.section}>
+        <Search />
+        {children}
+      </div>
+      {/* second section */}
+      <div></div>
+      {/* third section */}
+      <div></div>
+    </div>
   );
 };
 
