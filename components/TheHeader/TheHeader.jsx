2dc86086e2fefeb892fb9bf8228f669641282f4b
Vyachuk <Slavkoka045@gmail.com>
2023-11-10 13:36:50 +0200

    FIX: return to main worked header

--- components/TheHeader/TheHeader.jsx
+++ components/TheHeader/TheHeader.jsx
@@ -3,29 +3,56 @@ import Link from "next/link";
 import ButtonTheme from "../ButtonTheme/ButtonTheme";
 import { BsSearch } from "react-icons/bs";
 import { HiShoppingCart } from "react-icons/hi";
-import ListNav from "../ListNav/ListNav";
-import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import { RiMenu2Line } from "react-icons/ri";
 
 const TheHeader = () => {
   return (
-    <>
-      <header className={s.wrapper}>
-        <div className={s.navDiv}>
-          <ul className={s.iconsList}>
-            <li className={s.btnTheme}>
-              <ButtonTheme />
-            </li>
-            <li className={s.hoverIcon}>
-              <BsSearch className={s.headerIcon} />
-            </li>
-            <li className={s.hoverIcon}>
-              <HiShoppingCart className={s.headerIcon} />
-            </li>
-          </ul>
-          {/* <BurgerMenu /> */}
-        </div>
-      </header>
-    </>
+    <header className={s.wrapper}>
+      <nav className={s.navigation}>
+        {/* <RiMenu2Line className={s.burgerBtn} /> */}
+        <Link className={s.logo} href="/">
+          iShop
+        </Link>
+        <ul className={s.navList}>
+          <li className={s.navItem}>
+            <Link className={s.linkItem} href="/iphone">
+              Iphone
+            </Link>
+          </li>
+          <li className={s.navItem}>
+            <Link className={s.linkItem} href="/ipad">
+              Ipad
+            </Link>
+          </li>
+          <li className={s.navItem}>
+            <Link className={s.linkItem} href="/macbook">
+              Macbook
+            </Link>
+          </li>
+          <li className={s.navItem}>
+            <Link className={s.linkItem} href="/airpods">
+              Airpods
+            </Link>
+          </li>
+          <li className={s.navItem}>
+            <Link className={s.linkItem} href="/aboutUs">
+              Про нас
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <ul className={s.iconsList}>
+        <li className={s.btnTheme}>
+          <ButtonTheme />
+        </li>
+        <li className={s.hoverIcon}>
+          <BsSearch className={s.headerIcon} />
+        </li>
+        <li className={s.hoverIcon}>
+          <HiShoppingCart className={s.headerIcon} />
+        </li>
+      </ul>
+    </header>
   );
 };
 
