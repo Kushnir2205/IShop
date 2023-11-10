a33b6388292ffbe27167941c7f416523dc0cb834
Vyachuk <Slavkoka045@gmail.com>
2023-11-10 14:47:14 +0200

    ADD: modal burger menu

--- components/UI/BurgerMenuBtn/BurgerMenuBtn.jsx
+++ components/UI/BurgerMenuBtn/BurgerMenuBtn.jsx
@@ -1,10 +1,26 @@
+"use client";
+import { useState } from "react";
+import BurgerMenu from "@/components/BurgerMenu/BurgerMenu";
 import { RiMenu2Line } from "react-icons/ri";
 import s from "./BurgerMenuBtn.module.css";
+import { AiOutlineClose } from "react-icons/ai";
 
 const BurgerMenuBtn = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <>
-      <RiMenu2Line className={s.burgerBtn} />
+      <RiMenu2Line
+        className={s.burgerBtn}
+        onClick={() => setIsModalOpen((v) => !v)}
+      />
+      {isModalOpen && (
+        <div className={s.modalBackDrop}>
+          <AiOutlineClose
+            className={s.modalCloseBtn}
+            onClick={() => setIsModalOpen((v) => !v)}
+          />
+        </div>
+      )}
     </>
   );
 };
