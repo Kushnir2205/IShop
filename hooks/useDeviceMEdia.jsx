62be112ac7d3a24f2eec82bbde7ecf7c95709b26
Kushnir2205 <kushnirroman22@gmail.com>
2023-11-14 15:26:31 +0200

    add vercel json

--- hooks/useDeviceMEdia.jsx
+++ hooks/useDeviceMEdia.jsx
@@ -1,8 +1,13 @@
 "use client";
 import { screen } from "@/utils/screens";
+import { useEffect } from "react";
 import { useMedia } from "react-use";
 
 export function useDeviceMedia() {
+  // const screenWidth = window?.innerWidth;
+  // const isMobile = screenWidth < 744;
+  // const isTablet = screenWidth >= 744 && screenWidth < 1440;
+  // const isDesktop = screenWidth > 1440;
   const isMobile = useMedia(screen.breakpoints.mobile.media);
   const isTablet = useMedia(screen.breakpoints.tablet.media);
   const isDesktop = useMedia(screen.breakpoints.desktop.media);
