a1fb5ea99e2b81e726f0e80d4f39bb38f86aa6d7
Vyachuk <Slavkoka045@gmail.com>
2023-11-15 03:49:32 +0200

    ADD: update admin panel

--- components/ConsoleTable/ConsoleTable.jsx
+++ components/ConsoleTable/ConsoleTable.jsx
@@ -51,12 +51,12 @@ const ConsoleTable = ({ data }) => {
         size: 150,
         Cell: ({ renderedCellValue, row }) => (
           <Image
-            alt="avatar"
-            height={30}
-            width={30}
-            src={`https://ishop-backend-5skc.onrender.com/gudgets/2023-11-15T00:42:20.024Z-892534793_iPhone-15-Pro-Review-Featured-Gear.webp`}
+            alt={row.original.name}
+            height={65}
+            width={100}
+            src={row.original.poster[0]}
             loading="lazy"
-            style={{ borderRadius: "50%" }}
+            style={{ borderRadius: "20%" }}
           />
         ),
       },
@@ -76,10 +76,114 @@ const ConsoleTable = ({ data }) => {
 
   const table = useMaterialReactTable({
     columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    data,
+    enableColumnOrdering: true,
+    enableColumnPinning: true,
+    enableRowSelection: true,
+    enableRowActions: true,
+    // renderRowActionMenuItems: ({ closeMenu }) => [
+    //   <MenuItem
+    //     key={0}
+    //     onClick={() => {
+    //       // View profile logic...
+    //       closeMenu();
+    //     }}
+    //     sx={{ m: 0 }}
+    //   >
+    //     <ListItemIcon>
+    //       <AccountCircle />
+    //     </ListItemIcon>
+    //     View Profile
+    //   </MenuItem>,
+    //   <MenuItem
+    //     key={1}
+    //     onClick={() => {
+    //       // Send email logic...
+    //       closeMenu();
+    //     }}
+    //     sx={{ m: 0 }}
+    //   >
+    //     <ListItemIcon>
+    //       <Send />
+    //     </ListItemIcon>
+    //     Send Email
+    //   </MenuItem>,
+    // ],
+    // renderTopToolbar: ({ table }) => {
+    //   const handleDeactivate = () => {
+    //     table.getSelectedRowModel().flatRows.map((row) => {
+    //       alert("deactivating " + row.getValue("name"));
+    //     });
+    //   };
+
+    //   const handleActivate = () => {
+    //     table.getSelectedRowModel().flatRows.map((row) => {
+    //       alert("activating " + row.getValue("name"));
+    //     });
+    //   };
+
+    //   const handleContact = () => {
+    //     table.getSelectedRowModel().flatRows.map((row) => {
+    //       alert("contact " + row.getValue("name"));
+    //     });
+    //   };
+
+    //   return (
+    //     <Box
+    //       sx={(theme) => ({
+    //         backgroundColor: lighten(theme.palette.background.default, 0.05),
+    //         display: "flex",
+    //         gap: "0.5rem",
+    //         p: "8px",
+    //         justifyContent: "space-between",
+    //       })}
+    //     >
+    //       <Box sx={{ display: "flex", gap: "0.5rem", alignItems: "center" }}>
+    //         {/* import MRT sub-components */}
+    //         <MRT_GlobalFilterTextField table={table} />
+    //         <MRT_ToggleFiltersButton table={table} />
+    //       </Box>
+    //       <Box>
+    //         <Box sx={{ display: "flex", gap: "0.5rem" }}>
+    //           <Button
+    //             color="error"
+    //             disabled={!table.getIsSomeRowsSelected()}
+    //             onClick={handleDeactivate}
+    //             variant="contained"
+    //           >
+    //             Deactivate
+    //           </Button>
+    //           <Button
+    //             color="success"
+    //             disabled={!table.getIsSomeRowsSelected()}
+    //             onClick={handleActivate}
+    //             variant="contained"
+    //           >
+    //             Activate
+    //           </Button>
+    //           <Button
+    //             color="info"
+    //             disabled={!table.getIsSomeRowsSelected()}
+    //             onClick={handleContact}
+    //             variant="contained"
+    //           >
+    //             Contact
+    //           </Button>
+    //         </Box>
+    //       </Box>
+    //     </Box>
+    //   );
+    // },
+    // enableColumnFilterModes: true,
   });
 
-  return <MaterialReactTable table={table} />;
+  return (
+    <MaterialReactTable
+      table={table}
+      enableRowSelection
+      initialState={{ density: "compact" }}
+    />
+  );
 };
 
 export default ConsoleTable;
