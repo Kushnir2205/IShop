cbd2575fbd9d38fa92adfc297bc8e123da0bac6e
Vyachuk <Slavkoka045@gmail.com>
2023-11-22 20:31:43 +0200

    Change api

--- services/api/api.js
+++ services/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  // baseURL: "https://ishop-backend-5skc.onrender.com/api",
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://ishop-backend-5skc.onrender.com/api",
 });
 
 export const fetchAllGudget = async (params) => {
