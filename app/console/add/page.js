2eb9577033dc33b95e6c4c043d0fffcde42d8548
Vyachuk <Slavkoka045@gmail.com>
2023-11-27 22:49:57 +0200

    ADD: back btn to console

--- app/console/add/page.js
+++ app/console/add/page.js
@@ -1,9 +1,13 @@
 import ProductFormAction from "@/components/ProductFormAction/ProductFormAction";
+import Link from "next/link";
 import s from "./add.module.css";
 
 const Add = () => {
   return (
     <section className={s.section}>
+      <Link href={"/console"} className={s.backBtn}>
+        &lArr; Back
+      </Link>
       <h3 className={s.title}>Add product</h3>
       <ProductFormAction btnAction="Add product" />
     </section>
