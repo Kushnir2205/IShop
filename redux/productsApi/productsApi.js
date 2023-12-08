bcff1ce2133c93f352a9763cb9aca92d30913c76
Kushnir2205 <kushnirroman22@gmail.com>
2023-12-09 00:26:58 +0200

    add rtk query

--- redux/productsApi/productsApi.js
+++ redux/productsApi/productsApi.js
@@ -1,8 +1,38 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-export const productsApi = createApi({
-  reducerPath: "productsApi",
+export const api = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: "https://ishop-backend-5skc.onrender.com/api",
   }),
+  endpoints: (builder) => ({
+    fetchAllGudget: builder.query({
+      query: (params) => "store",
+    }),
+    fetchById: builder.query({
+      query: (gudgetId) => `store/${gudgetId}`,
+    }),
+    fetchAddGudget: builder.mutation({
+      query: (info) => ({
+        url: "store",
+        method: "POST",
+        body: info,
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }),
+    }),
+    fetchDeleteItem: builder.mutation({
+      query: (gudgetId) => ({
+        url: `store/${gudgetId}`,
+        method: "DELETE",
+      }),
+    }),
+  }),
 });
+
+export const {
+  useFetchAllGudgetQuery,
+  useFetchByIdQuery,
+  useFetchAddGudgetMutation,
+  useFetchDeleteItemMutation,
+} = api;
--- services/api/api.js
+++ services/api/api.js
@@ -1,41 +1,41 @@
-import axios from "axios";
+// import axios from "axios";
 
-const instance = axios.create({
-  baseURL: "https://ishop-backend-5skc.onrender.com/api",
-});
+// const instance = axios.create({
+//   baseURL: "https://ishop-backend-5skc.onrender.com/api",
+// });
 
-export const fetchAllGudget = async (params) => {
-  const { data } = await instance.get("/store", { params });
-  return data;
-};
+// export const fetchAllGudget = async (params) => {
+//   const { data } = await instance.get("/store", { params });
+//   return data;
+// };
 
-export const fetchById = async (gudgetId) => {
-  const { data } = await instance.get(`/store/${gudgetId}`);
-  return data;
-};
+// export const fetchById = async (gudgetId) => {
+//   const { data } = await instance.get(`/store/${gudgetId}`);
+//   return data;
+// };
 
-export const fetchAddGudget = async (info) => {
-  const form = new FormData();
+// export const fetchAddGudget = async (info) => {
+//   const form = new FormData();
 
-  for (var key in info) {
-    if (key === "poster" && Array.isArray(info[key])) {
-      info[key].forEach((file, index) => {
-        form.append(key, file);
-      });
-    } else {
-      form.append(key, info[key]);
-    }
-  }
+//   for (var key in info) {
+//     if (key === "poster" && Array.isArray(info[key])) {
+//       info[key].forEach((file, index) => {
+//         form.append(key, file);
+//       });
+//     } else {
+//       form.append(key, info[key]);
+//     }
+//   }
 
-  const { data } = await instance.post("/store", form, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-    },
-  });
-  return data;
-};
+//   const { data } = await instance.post("/store", form, {
+//     headers: {
+//       "Content-Type": "multipart/form-data",
+//     },
+//   });
+//   return data;
+// };
 
-export const fetchDeleteItem = async (gudgetId) => {
-  const { data } = await instance.delete(`/store/${gudgetId}`);
-  return data;
-};
+// export const fetchDeleteItem = async (gudgetId) => {
+//   const { data } = await instance.delete(`/store/${gudgetId}`);
+//   return data;
+// };
